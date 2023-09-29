767c4acb2fe19ee480f33ebc66ba1ae8d03deae2
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-09-29 13:48:07 +0100

    majority element - Boyer-Moore Voting Algorithm

--- majority-element/majority_element.go
+++ majority-element/majority_element.go
@@ -8,6 +8,8 @@ func main() {
 	majorityElement([]int{2, 2, 1, 1, 1, 2, 2})
 }
 
+// this soltution 0(n) for time and complexity as its dependent on the length of the array
+// space is 0(n) because the map size is deendent on the array size
 func majorityElement(nums []int) int {
 
 	majority := (len(nums) / 2)
@@ -26,4 +28,31 @@ func majorityElement(nums []int) int {
 		}
 	}
 	return 0
+}
+
+//Could you solve the problem in linear time and in O(1) space?
+//Boyer-Moore Voting Algorithm.
+
+//Time is still 0(n) as we have to loop through the array of size n
+// but space is 0(1) because it ses only two variables: candidate and count.
+// These variables do not depend on the size of the input array.
+// They remain constant regardless of the input size.
+
+func majorityElementImproved(nums []int) int {
+	candidate := nums[0]
+	count := 1
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == candidate {
+			count++
+		} else {
+			count--
+			if count == 0 {
+				candidate = nums[i]
+				count = 1
+			}
+		}
+	}
+
+	return candidate
 }
--- majority-element/majority_element_test.go
+++ majority-element/majority_element_test.go
@@ -26,3 +26,24 @@ func TestMajorityElement(t *testing.T) {
 		assert.Equal(t, 2, output)
 	})
 }
+
+func TestMajorityElementImproved(t *testing.T) {
+	t.Run("case 1", func(t *testing.T) {
+		input := []int{3, 2, 3}
+		output := majorityElementImproved(input)
+
+		assert.Equal(t, 3, output)
+	})
+	t.Run("case 2", func(t *testing.T) {
+		input := []int{0, 0, 1, 1, 1, 1, 1, 1, 2, 3, 3}
+		output := majorityElementImproved(input)
+
+		assert.Equal(t, 1, output)
+	})
+	t.Run("case 3", func(t *testing.T) {
+		input := []int{2, 2, 1, 1, 1, 2, 2}
+		output := majorityElementImproved(input)
+
+		assert.Equal(t, 2, output)
+	})
+}
