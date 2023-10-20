72032c0cc7f4b4b47b7a8bbb1f9f18b04a019127
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-10-20 09:30:00 +0100

    buy-sell-stock-2

--- buy-sell-stock/buy_sell_stock.go
+++ buy-sell-stock/buy_sell_stock.go
@@ -6,10 +6,6 @@ import "math"
 //You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 //Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
-func main() {
-	maxProfit([]int{7, 1, 5, 3, 6, 4})
-}
-
 func maxProfit(prices []int) int {
 	minPrice := math.MaxInt
 	maxProfit := 0
--- buy-sell-stock/buy_sell_stock_II_test.go
+++ buy-sell-stock/buy_sell_stock_II_test.go
@@ -6,12 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test(t *testing.T) {
+func Test2(t *testing.T) {
 	t.Run("case 1", func(t *testing.T) {
 		input := []int{7, 1, 5, 3, 6, 4}
-		expectedOutput := 5
+		expectedOutput := 7
 
-		output := maxProfit(input)
+		output := maxProfit2(input)
 
 		assert.Equal(t, expectedOutput, output)
 	})
@@ -19,7 +19,7 @@ func Test(t *testing.T) {
 		input := []int{2, 4, 1}
 		expectedOutput := 2
 
-		output := maxProfit(input)
+		output := maxProfit2(input)
 
 		assert.Equal(t, expectedOutput, output)
 	})
@@ -28,7 +28,15 @@ func Test(t *testing.T) {
 		input := []int{7, 6, 4, 3, 1}
 		expectedOutput := 0
 
-		output := maxProfit(input)
+		output := maxProfit2(input)
+
+		assert.Equal(t, expectedOutput, output)
+	})
+	t.Run("case 3", func(t *testing.T) {
+		input := []int{1, 2, 3, 4, 5}
+		expectedOutput := 4
+
+		output := maxProfit2(input)
 
 		assert.Equal(t, expectedOutput, output)
 	})
