c4788cfd5013088281fb286e1fc33da455e850f2
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-10-23 10:03:21 +0100

    word-pattern

---
+++ word-pattern/word_pattern.go
@@ -0,0 +1,46 @@
+// Given a pattern and a string s, find if s follows the same pattern.
+// Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+// pattern contains only lower-case English letters.
+// s contains only lowercase English letters and spaces ' '.
+// s does not contain any leading or trailing spaces.
+// All the words in s are separated by a single space.
+
+package main
+
+import (
+	"strings"
+)
+
+func main() {
+	wordPattern("abba", "dog cat cat dog")
+}
+
+func wordPattern(pattern string, s string) bool {
+	pToS := make(map[byte]string)
+	sToP := make(map[string]byte)
+
+	stringArray := strings.Split(s, " ")
+
+	if len(pattern) != len(stringArray) {
+		return false
+	}
+
+	for i := 0; i < len(pattern); i++ {
+		patternChar, sChar := pattern[i], stringArray[i]
+
+		value, exists := pToS[patternChar]
+		if exists && value != sChar {
+			return false
+		} else {
+			pToS[patternChar] = sChar
+		}
+
+		v, e := sToP[sChar]
+		if e && v != patternChar {
+			return false
+		} else {
+			sToP[sChar] = patternChar
+		}
+	}
+	return true
+}
---
+++ word-pattern/word_pattern_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+func Test(t *testing.T) {
+	t.Run("case 1", func(t *testing.T) {
+		output := wordPattern("abba", "dog cat cat dog")
+
+		assert.True(t, output)
+	})
+	t.Run("case 2", func(t *testing.T) {
+		output := wordPattern("abba", "dog cat cat fish")
+
+		assert.False(t, output)
+	})
+	t.Run("case 3", func(t *testing.T) {
+		output := wordPattern("aaaa", "dog cat cat dog")
+
+		assert.False(t, output)
+	})
+	t.Run("case 4", func(t *testing.T) {
+		output := wordPattern("abba", "dog dog dog")
+
+		assert.False(t, output)
+	})
+	t.Run("case 5, incorrect lengths", func(t *testing.T) {
+		output := wordPattern("aaaaa", "dog cat cat dog")
+
+		assert.False(t, output)
+	})
+}
