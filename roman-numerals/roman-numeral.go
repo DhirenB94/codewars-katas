db0b13d02a4455cd00e0b88419010278f1979a69
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-10-28 21:32:18 +0100

    intToRoman

--- roman-numerals/roman-numeral.go
+++ roman-numerals/roman-numeral.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(romanToInt("MCMXCIV"))
+	// fmt.Println(romanToInt("MCMXCIV"))
+	fmt.Println(intToRoman(20))
+
 }
 
 var romanNumerals = map[string]int{
@@ -72,4 +74,93 @@ func romanToInt(s string) int {
 		return combined + romanToInt(s[2:])
 	}
 	return firstNumeral + romanToInt(s[1:])
+}
+
+// var intRomansNumnerals = map[int]string{
+// 	1: "I",
+// 	4: "IV",
+// 	5: "V",
+// 	9: "IX",
+// 	10: "X",
+// 	40: "XL",
+// 	50: "L",
+// 	90: "XC",
+// 	100: "C",
+// 	400: "CD",
+// 	500: "D",
+// 	900: "CM",
+// 	1000: "M",
+// }
+
+func intToRoman(num int) string {
+	romNumeral := ""
+
+	for num > 0 {
+		if num >= 1000 {
+			romNumeral += "M"
+			num -= 1000
+			continue
+		}
+		if num >= 900 {
+			romNumeral += "CM"
+			num -= 900
+			continue
+		}
+		if num >= 500 {
+			romNumeral += "D"
+			num -= 500
+			continue
+		}
+		if num >= 400 {
+			romNumeral += "CD"
+			num -= 400
+			continue
+		}
+		if num >= 100 {
+			romNumeral += "C"
+			num -= 100
+			continue
+		}
+		if num >= 90 {
+			romNumeral += "XC"
+			num -= 90
+			continue
+		}
+		if num >= 50 {
+			romNumeral += "L"
+			num -= 50
+			continue
+		}
+		if num >= 40 {
+			romNumeral += "XL"
+			num -= 40
+			continue
+		}
+		if num >= 10 {
+			romNumeral += "X"
+			num -= 10
+			continue
+		}
+		if num >= 9 {
+			romNumeral += "IX"
+			num -= 9
+			continue
+		}
+		if num >= 5 {
+			romNumeral += "V"
+			num -= 5
+			continue
+		}
+		if num >= 4 {
+			romNumeral += "IV"
+			num -= 4
+			continue
+		}
+		if num >= 1 {
+			romNumeral += "I"
+			num -= 1
+			continue
+		}
+	}
+	return romNumeral
 }
