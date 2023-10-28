67382e16155e57096372f48d1359312526e4e569
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-10-28 20:56:04 +0100

    roman numeral alt (longer) method

--- roman-numerals/roman-numeral.go
+++ roman-numerals/roman-numeral.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(romanNumeralConverter("XXI"))
+	fmt.Println(romanToInt("MCMXCIV"))
 }
 
-var numeralMap = map[string]int{
+var romanNumerals = map[string]int{
 	"I": 1,
 	"V": 5,
 	"X": 10,
@@ -16,19 +16,60 @@ var numeralMap = map[string]int{
 	"M": 1000,
 }
 
-func romanNumeralConverter(romanNumerals string) int {
-	if len(romanNumerals) == 0 {
+func romanToInt(s string) int {
+	// total := 0
+	// for i := 0; i < len(s); i++ {
+	// 	//last pos
+	// 	if i == len(s)-1 {
+	// 		total = total + romanNumerals[string(s[i])]
+	// 		break
+	// 	}
+	// 	if string(s[i]) == "I" && string(s[i+1]) == "V" {
+	// 		total = total + 4
+	// 		i++
+	// 		continue
+	// 	}
+	// 	if string(s[i]) == "I" && string(s[i+1]) == "X" {
+	// 		total = total + 9
+	// 		i++
+	// 		continue
+	// 	}
+	// 	if string(s[i]) == "X" && string(s[i+1]) == "L" {
+	// 		total = total + 40
+	// 		i++
+	// 		continue
+	// 	}
+	// 	if string(s[i]) == "X" && string(s[i+1]) == "C" {
+	// 		total = total + 90
+	// 		i++
+	// 		continue
+	// 	}
+	// 	if string(s[i]) == "C" && string(s[i+1]) == "D" {
+	// 		total = total + 400
+	// 		i++
+	// 		continue
+	// 	}
+	// 	if string(s[i]) == "C" && string(s[i+1]) == "M" {
+	// 		total = total + 900
+	// 		i++
+	// 		continue
+	// 	}
+	// 	total = total + romanNumerals[string(s[i])]
+	// }
+
+	// return total
+	if len(s) == 0 {
 		return 0
 	}
 
-	firstNumeral := numeralMap[string(romanNumerals[0])]
-	if len(romanNumerals) == 1 {
+	firstNumeral := romanNumerals[string(s[0])]
+	if len(s) == 1 {
 		return firstNumeral
 	}
-	nextNumeral := numeralMap[string(romanNumerals[1])]
+	nextNumeral := romanNumerals[string(s[1])]
 	if nextNumeral > firstNumeral {
 		combined := nextNumeral - firstNumeral
-		return combined + romanNumeralConverter(romanNumerals[2:])
+		return combined + romanToInt(s[2:])
 	}
-	return firstNumeral + romanNumeralConverter(romanNumerals[1:])
+	return firstNumeral + romanToInt(s[1:])
 }
